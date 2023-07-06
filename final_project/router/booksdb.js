903930aa978494f6c5c71e46f10470c2faf84b56
Thunder-Flag <harshbhatia0007@gmail.com>
2023-07-06 11:17:17 -0400

    done upto task 14
    Signed-off-by: Thunder-Flag <harshbhatia0007@gmail.com>

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,67 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: 
+        {
+            "author": "Chinua Achebe",
+            "title": "Things Fall Apart", 
+            "review": "cool book"
+        },
+
+      2: 
+        {
+          "author": "Hans Christian Andersen",
+          "title": "Fairy tales", 
+          "review": "easy read"
+        },
+
+      3: 
+        {
+          "author": "Dante Alighieri",
+          "title": "The Divine Comedy", 
+          "review": "delightful" 
+        },
+      4: 
+        {
+          "author": "Unknown",
+          "title": "The Epic Of Gilgamesh", 
+          "review": "powerful dialogue"
+        },
+      5: 
+        {
+          "author": "Unknown",
+          "title": "The Book Of Job", 
+          "review": "recommended 9/10" 
+        },
+      6: 
+        {
+          "author": "Unknown",
+          "title": "One Thousand and One Nights", 
+          "review": "Engaging 7/10" 
+        },
+      7: 
+        {
+          "author": "Unknown",
+          "title": "Nj\u00e1l's Saga", 
+          "review": "Not very interesting 5/10" 
+        },
+      8: 
+        {
+          "author": "Jane Austen",
+          "title": "Pride and Prejudice", 
+          "review": "revolutionary" 
+        },
+      9: 
+        {
+          "author": "Honor\u00e9 de Balzac",
+          "title": "Le P\u00e8re Goriot", 
+          "review": "incredible" 
+        },
+      10: 
+        {
+          "author": "Samuel Beckett",
+          "title": "Molloy, Malone Dies, The Unnamable, the trilogy", 
+          "review": "fun read" 
+        }
 }
 
 module.exports=books;
+
